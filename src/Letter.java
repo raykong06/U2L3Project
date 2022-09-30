97205cd0baff875c66f2cecf29a0a37b51f7c975
BT_4N2_24 <rkong2588@bths.edu>
2022-09-30 11:43:01 -0400

    added Letter class and LetterWriter class with filled in writeLetter method

---
+++ src/Letter.java
@@ -0,0 +1,27 @@
+public class Letter
+{
+    /** Prints out a complete letter by printing the greeting,
+     Followed by the special message, followed by the closing */
+
+    public void writeLetter()
+    {
+        greeting();
+        specialMessage();
+        closing();
+    }
+
+    public void greeting()
+    {
+        System.out.println("Hello, friend!");
+    }
+
+    public void specialMessage()
+    {
+        System.out.println("Java is pretty cool, wouldn't you say?");
+    }
+
+    public void closing()
+    {
+        System.out.println("See you later!");
+    }
+}
---
+++ src/LetterWriter.java
@@ -0,0 +1,8 @@
+public class LetterWriter
+{
+    public static void main(String[] args)
+    {
+        Letter letter = new Letter();
+        letter.writeLetter();
+    }
+}
